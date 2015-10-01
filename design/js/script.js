70f5f97bf67b937730259bcde4192de71d21be27
Anna Courchaine <askccourchaine@gmail.com>
2015-09-30 20:55:13 -0500

    First commit
    test

---
+++ design/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+			$(function(){
+  				$('#gallery').mixItUp({
+					load: {
+						sort: 'random'
+						},
+					animation: {
+						enable: false
+					}
+				});
+			});	
+
+	
+});
