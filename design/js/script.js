9fd92f721ffe17364cfd3808b0f772a7ab1390ce
Anna Courchaine <askccourchaine@gmail.com>
2015-11-18 19:55:22 -0600

    COMMIT
    wefa

--- design/js/script.js
+++ design/js/script.js
@@ -42,7 +42,16 @@ $(document).ready(function() {
 					
 	new ScrollMagic.Scene({triggerElement: "#parallax3"})
 					.setTween("#parallax3 > div", {y: "10%", ease: Linear.easeNone})
-					.addTo(controller);		
+					.addTo(controller);	
+					
+		$(window).on("resize", function(e) {
+	  if ($(window).width() < 768 && controller.enabled()) {
+		controller.enabled(false);
+	  } else if (!controller.enabled()) {
+		controller.enabled(true);
+	  }
+	});				
+						
 	});
 	
 //responsive
